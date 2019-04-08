9d88caa40a6a5f66826f26dbe11119c638f44552
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-08 09:45:27 +0300

    created blueprint for logging in and creating an account

--- p_locker.py
+++ p_locker.py
@@ -1,5 +1,18 @@
 def main():
-    print("shie")
+    print("************PASSWORD LOCKER**************")
+    while True:
+        print(' ')
+        print("*" * 70)
+        print('PLease choose an option: \n 1 to Create an Account \n 2 to Log In \n X to Exit')
+        code = input('Enter a choice: ').upper().strip()
+        if code == 'X':
+            break
+        elif code == "1":
+            print("create an account")
+        elif code == "2":
+            print("Log in")
+        else:
+            print("Please enter a valid input")
 
 
 if __name__ == '__main__':
