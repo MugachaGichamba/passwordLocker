22e3733b465f20db4f0621e092e00bd2483e801a
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-08 10:13:29 +0300

    created functionality to register a new user

--- p_locker.py
+++ p_locker.py
@@ -1,5 +1,21 @@
+from user import User
+
+
+def create_user(names,password):
+
+	'''
+	Function to create a new user account
+	'''
+	new_user = User(names,password)
+	return new_user
+
+
 def main():
+
     print("************PASSWORD LOCKER**************")
+    """
+    loop to continually ask for input until it breaks
+    """
     while True:
         print(' ')
         print("*" * 70)
@@ -8,7 +24,12 @@ def main():
         if code == 'X':
             break
         elif code == "1":
-            print("create an account")
+            print("Please create your account below")
+            names = input("Enter your names seperated by a space")
+            password = input("Please Enter your password")
+            User.saveUser(create_user(names, password))
+            print("successfuly created an account for " + names + " password: " + password)
+
         elif code == "2":
             print("Log in")
         else:
