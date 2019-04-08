27be394973ead5f9fc17147ed0ba72c4941cceed
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-08 09:23:07 +0300

    created function to save a user

--- user.py
+++ user.py
@@ -1,7 +1,16 @@
 class User:
     """
     blueprint for user accounts
+
     """
+    users = []
+
     def __init__(self, names,  password ):
         self.names = names
         self.password = password
+
+    def saveUser(self):
+        """
+        save a newly created user
+        """
+        User.users.append(self);
